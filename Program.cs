bcc77125e97ea506a7af03833fd0ad32387767ba
Vakil <burangulov.vakil@gmail.com>
2025-06-01 18:18:49 +0300

    add premium account

--- Program.cs
+++ Program.cs
@@ -4,5 +4,16 @@ class Program
 {
     static void Main()
     {
+        IAccount salaryAccount = new SalaryAccount() {Balance = 1000, Interest = 2};
+        IAccount ordinaryAccount = new OrdinaryAccount() {Balance = 1000, Interest = 2};
+        IAccount premiumAccount = new PremiumAccount() {Balance = 1000, Interest = 2};
+        
+        salaryAccount.CalculateInterest();
+        ordinaryAccount.CalculateInterest();
+        premiumAccount.CalculateInterest();
+        
+        Console.WriteLine(salaryAccount.Interest);
+        Console.WriteLine(ordinaryAccount.Interest);
+        Console.WriteLine(premiumAccount.Interest);
     }
 }
